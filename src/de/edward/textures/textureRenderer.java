cba69897533551c8b25d3b3e88743ba4cf14b7bf
TheBoringEdward <querulator78@gmail.com>
2022-05-15 11:07:46 +0200

    Textures now use .png files

--- src/de/edward/textures/textureRenderer.java
+++ src/de/edward/textures/textureRenderer.java
@@ -17,38 +17,60 @@ public class textureRenderer {
         this.PosY = PosY;
     }
 
-    textureDir tM = new textureDir();
 
     public void drawTexture(Graphics g, char t){
-        for (int i = 0; i < 6; i++){
-            for (int j = 0; j < 6; j++) {
-                switch (tM.fetchTexture(t, j).charAt(i)) {
-                    default -> g.setColor(Color.decode("#ba55d3")); // purple
-                    case 'b' -> g.setColor(Color.black);
-                    case 'r' -> g.setColor(Color.red);
-                    case 'w' -> g.setColor(Color.white);
-                    case 'p' -> g.setColor(Color.decode("#ba55d3")); // purple
-                    case '2' -> g.setColor(Color.decode("#a52a2a")); // dark brown
-                    case '3' -> g.setColor(Color.decode("#d2691e")); // brown
-                    case '4' -> g.setColor(Color.decode("#808080")); // grey
-                    case '5' -> g.setColor(Color.decode("#676767")); // dark grey
-                    case '6' -> g.setColor(Color.decode("#353535")); // darker grey
+                switch (t) {
+                    default -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/missing.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case ' ' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/void.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case '4' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/plank.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case '5' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/stone-brick.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case '6' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/upper-door.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case '7' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/lower-door.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    case '8' -> {
+                        try {
+                            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/stone-wall.png"));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
-                g.fillRect(PosX + (5 * i), PosY + (5 * j), 5, 5);
-            }
-        }
-        try {
-            img = ImageIO.read(new File("/home/edward/IdeaProjects/Movable/src/de/edward/textures/small-bookshelf.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        g.drawImage(img, 100 + (30 * 4), 100, null);
-        g.drawImage(img, 100 + (30 * 5), 100, null);
-
-        /*
-        g.setColor(Color.blue);
-        g.drawRect(125,125,50,50);
-         */
+                g.drawImage(img, PosX, PosY, null);
     }
 
 }
